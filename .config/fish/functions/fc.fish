0c32f69ff8f8d6a474a0eb4749914ffbaa79ee49
chrislloyd <christopher.lloyd@gmail.com>
2009-08-01 00:03:44 -0700

    fc matches all project names now.

--- .config/fish/functions/fc.fish
+++ .config/fish/functions/fc.fish
@@ -1,5 +1,5 @@
 function fc
-  set -l PROJECT (echo -n $argv[1] | egrep -io '/[[:alnum:]]+\.git' | sed -E 's/\/|\.git//g')
+  set -l PROJECT (echo -n $argv[1] | egrep -io '/([^/]+)\.git$' | sed -E 's/\/|\.git//g')
   cd $PROJECTS
   git clone $argv
   cd $PROJECT
