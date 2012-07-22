a4e9379630729d55d098d1067893c4364b566d09
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-07-22 18:27:26 +0200

    C++ and Gud mode

---
+++ emacs.d/setup-gdb.el
@@ -0,0 +1,22 @@
+;;; setup-gdb.el --- GDB and GUD setup
+
+;; General GDB and GUD settings
+(add-hook 'gdb-mode-hook '(lambda ()
+                            (gud-tooltip-mode 1)
+                            (gdb-many-windows t)
+                            (tool-bar-mode t)
+                            (fullscreen)
+                            (gdb-show-changed-values t)))
+(setq gdb-many-windows t)
+
+;; Key bindings for GUD
+(defun my-gud-mode-hook ()
+  (define-key gud-mode-map (kbd "S-<f9>") 'gud-go)
+  (define-key gud-mode-map (kbd "C-c w") 'gud-self-watch)
+  (define-key gud-mode-map (kbd "C-c e") 'gud-print)
+  (define-key gud-mode-map (kbd "<f7>") 'gud-step)
+  (define-key gud-mode-map (kbd "<f8>") 'gud-next))
+
+(add-hook 'gud-mode 'my-gud-mode-hook)
+
+(provide 'setup-gdb)
