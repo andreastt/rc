caff23aa6e81a040365d4b7d3f0c1f9fc34f51ec
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-02-20 11:08:50 +0100

    C-z resumes program from command-line

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -3,3 +3,6 @@ bindkey "\e[5C" forward-word
 bindkey "\e0c" emacs-forward-word
 bindkey "\e[5D" backward-word
 bindkey "\e0d" emacs-backward-word
+
+# C-z resumes program by executing `fg` on command line
+bindkey -s "^Z" 'fg\n'
