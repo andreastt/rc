b8799fad17c4af3f130db2d84e946baef08e0584
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-06-07 00:14:29 +0200

    Move misc to sane-defaults

--- emacs.d/sane-defaults.el
+++ emacs.d/sane-defaults.el
@@ -108,4 +108,18 @@
 (setq auto-save-interval 20   ;; n keystroke(s)
       auto-save-timeout 10)  ;; ten idle seconds
 
+;; Seed the random number generator
+(random t)
+
+;; Whitespace
+(setq whitespace-style '(trailing lines space-before-tab
+                                  indentation space-after-tab)
+      whitespace-line-column 100)
+
+;; Various superfluous white space
+;(add-hook 'before-save-hook 'cleanup-buffer-safe)
+
+;; Enable console mouse
+(setq xterm-mouse-mode t)
+
 (provide 'sane-defaults)
