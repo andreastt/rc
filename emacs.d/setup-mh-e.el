f4bf900b1b3bcbc71ff8ecc49b1de51d0fb8c821
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-06-25 00:00:26 +0200

    Set up mh-e

---
+++ emacs.d/setup-mh-e.el
@@ -0,0 +1,6 @@
+;; Make mh-e the default MUA for sending and reading mail
+(setq mail-user-agent 'mh-e-user-agent
+      read-mail-command 'mh-rmail)
+
+;;(setq mh-send-uses-spost-flag t
+(setq mail-header-separator "--------")
