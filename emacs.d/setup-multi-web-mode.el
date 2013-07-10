ee3cdbf75c77b72b79d3efce11529e59620c401c
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-07-10 16:54:45 +0200

    Mako templates should be treated by multi-web-mode

--- emacs.d/setup-multi-web-mode.el
+++ emacs.d/setup-multi-web-mode.el
@@ -3,7 +3,7 @@
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
                   (js-mode "<script[^>]*>" "</script>")
                   (css-mode "<style[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5" "mak"))
 (multi-web-global-mode 1)
 
 (provide 'setup-multi-web-mode)
