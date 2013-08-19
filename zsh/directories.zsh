b1584db0ad0d2ae6ae4a9c70c7bb88fbf70ee77e
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-08-19 19:04:48 +0200

    Change GOPATH when a .gopath file is present in directory

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -2,3 +2,11 @@
 setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
+
+# Pick up .goproject files to set GOPATH
+function chpwd() {
+    if [ -r $PWD/.gopath ]; then
+        echo "changing GOPATH"
+        export GOPATH=$PWD
+    fi
+}
