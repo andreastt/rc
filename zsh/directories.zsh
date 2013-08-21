33c04df1bdcc4d146a764cbcca2fb23a35dd1c4a
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-08-21 09:36:40 +0200

    Improved GOPATH setting and unsetting

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -3,9 +3,27 @@ setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
 
-# Pick up .gopath files to set GOPATH
+# Update GOPATH depending on which Go project you're currently
+# traversing.
+#
+# Sets environmental variable GOPATH if any parent directories has
+# both a .gopath file and a ./src directory in it.  Correspondingly it
+# unsets the variable if the previous condition is not true.
+function update_gopath() {
+    c="$PWD"
+
+    while [[ "`/bin/readlink -f $c`" != "/" ]];
+    do
+        if [ -f $c/.gopath ] && [ -d $c/src ]; then
+            export GOPATH=$PWD
+            return
+        fi
+        c=${c}/..
+    done
+
+    unset GOPATH
+}
+
 function chpwd() {
-    if [ -r $PWD/.gopath ]; then
-        export GOPATH=$PWD
-    fi
+    update_gopath
 }
