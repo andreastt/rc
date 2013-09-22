fd64e869390d4fca5ca1b29527aba1febc96d560
Andreas Tolf Tolfsen <andreastt@opera.com>
2013-09-22 15:14:54 +0200

    Various C mode improvements

--- emacs.d/setup-c-mode.el
+++ emacs.d/setup-c-mode.el
@@ -1,12 +1,31 @@
-(add-hook 'gtags-mode-hook 
-          (lambda()
-            (local-set-key (kbd "M-.") 'gtags-find-tag)     ; find a tag, also M-.
-            (local-set-key (kbd "M-,") 'gtags-find-rtag)))  ; reverse tag
+;; global is used for looking up tags and symbols.
+(defun my-gtags-hook ()
+  ;; Find a tag
+  (local-set-key (kbd "M-.") 'gtags-find-tag)
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (require 'gtags)
-            (gtags-mode t)
-            (gtags-create-or-update)))
+  ;; Find reverse tag
+  (local-set-key (kbd "M-,") 'gtags-find-rtag))
+(add-hook 'gtags-mode-hook 'my-gtags-hook)
+
+;; Change scroll behaviour of compilation
+(setq compilation-scroll-output 'first-error)
+
+(defun my-c-mode-hook ()
+  ;; Compile
+  (define-key c-mode-map (kbd "C-<f9>") 'compile)
+
+  ;; Kill compilation
+  (define-key c-mode-map (kbd "C-<f2>") 'kill-compilation)
+
+  ;; Debug with gud-gdb
+  (define-key c-mode-map (kbd "S-<f9>") 'gdb)
+
+  ;; Switch between header and implementation
+  (define-key c-mode-map (kbd "<f12>") 'ff-find-other-file)
+
+  (require 'gtags)
+  (gtags-mode t)
+  (gtags-create-or-update))
+(add-hook 'c-mode-common-hook 'my-c-mode-hook)
 
 (provide 'setup-c-mode)
