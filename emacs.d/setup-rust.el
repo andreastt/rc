a2ee82d77b8c303c2e5eabe18958f1d0706e7354
Andreas Tolfsen <andreastt@opera.com>
2013-09-30 00:17:34 +0200

    Just require it

--- emacs.d/setup-rust.el
+++ emacs.d/setup-rust.el
@@ -1,2 +1,6 @@
-(autoload 'rust-mode "rust-mode" t)
+;; rust-mode.el does autoloading and mode mappings internally.  This
+;; statement is only an explicit require since we can't trust
+;; everything in emacs.d/vendor.
+(require 'rust-mode)
+
 (provide 'setup-rust)
