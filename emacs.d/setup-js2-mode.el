0e92882685e167399742a5eca495487deb12aa7a
Andreas Tolfsen <ato@mozilla.com>
2013-11-22 01:01:15 +0000

    gtags experiment

--- emacs.d/setup-js2-mode.el
+++ emacs.d/setup-js2-mode.el
@@ -1,16 +1,33 @@
-(setq-default js2-allow-rhino-new-expr-initializer nil)
-(setq-default js2-auto-indent-p nil)
-(setq-default js2-enter-indents-newline nil)
-(setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname"))
-(setq-default js2-idle-timer-delay 0.1)
-(setq-default js2-indent-on-enter-key nil)
-(setq-default js2-mirror-mode nil)
-(setq-default js2-strict-inconsistent-return-warning nil)
-(setq-default js2-auto-indent-p t)
-(setq-default js2-rebind-eol-bol-keys nil)
-(setq-default js2-include-rhino-externs nil)
-(setq-default js2-include-gears-externs nil)
-(setq-default js2-concat-multiline-strings 'eol)
+(defun my-gtags-hook ()
+  ;; Find a tag
+  (local-set-key (kbd "M-.") 'gtags-find-tag)
+
+  ;; Find reverse tag
+  (local-set-key (kbd "M-,") 'gtags-find-rtag))
+(add-hook 'gtags-mode-hook 'my-gtags-hook)
+
+(defun my-js-hook ()
+  (require 'gtags)
+  (gtags-mode t)
+  (gtags-create-or-update))
+(add-hook 'js-mode-hook 'my-js-hook)
+
+(require 'gtags)
+(gtags-mode t)
+
+(setq-default js2-allow-rhino-new-expr-initializer nil
+              js2-auto-indent-p nil
+              js2-enter-indents-newline nil
+              js2-global-externs '("module" "require" "jQuery" "$" "_" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname")
+              js2-idle-timer-delay 0.1
+              js2-indent-on-enter-key nil
+              js2-mirror-mode nil
+              js2-strict-inconsistent-return-warning nil
+              js2-auto-indent-p t
+              js2-rebind-eol-bol-keys nil
+              js2-include-rhino-externs nil
+              js2-include-gears-externs nil
+              js2-concat-multiline-strings 'eol)
 
 (autoload 'js2-mode "js2-mode" t)
 (autoload 'js2-imenu-extras "js2-imenu-extras" t)
