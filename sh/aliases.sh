c2a142414c477e05b855b22639a1583c28398205
Andreas Tolfsen <ato@mozilla.com>
2013-11-28 18:18:08 +0000

    Correct aliases for Linux

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -25,10 +25,10 @@ alias l="ls -lah"
 alias sl=ls
 
 # Commonly used programs
-alias emacs="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
-alias e="emacs -c"
-alias en="emacs -nw"
-alias ew="emacs --no-wait -c"
+#alias emacs="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
+alias e="emacsclient -c"
+alias en="emacsclient -nw"
+alias ew="emacsclient --no-wait -c"
 
 # Other machines
 alias njord="ssh -YC ato@sny.no"
