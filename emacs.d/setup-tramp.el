33197f45b707fd87599d79357468358d2a104b82
Andreas Tolfsen <ato@mozilla.com>
2014-01-11 17:24:05 +0100

    Use scp as it's apparently faster, might not be for large files

--- emacs.d/setup-tramp.el
+++ emacs.d/setup-tramp.el
@@ -1,8 +1,8 @@
 (autoload 'password-cache "password-cache" t)
 (autoload 'tramp "tramp" t)
 
-(setq password-cache-expiry nil      ;; If required to enter password,
-                                     ;; save it forever
-      tramp-default-method "rsync")  ;; Uses ControlMaster auto
+(setq password-cache-expiry nil    ;; If required to enter password,
+                                   ;; save it forever
+      tramp-default-method "scp")  ;; Uses ControlMaster auto
 
 (provide 'setup-tramp)
