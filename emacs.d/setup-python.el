58cbe490210525d47006f979c13ee27521d0e7a0
Andreas Tolfsen <ato@mozilla.com>
2014-01-21 11:37:32 +0000

    Make flymake-mode in Python work again

--- emacs.d/setup-python.el
+++ emacs.d/setup-python.el
@@ -6,27 +6,20 @@
       python-shell-completion-module-string-code "';'.join(module_completion('''%s''')\n"
       python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
-;; PEP8 check on the fly
+;; On the fly code checking
 (when (load "flymake" t)
- (defun flymake-pylint-init ()
-   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                      'flymake-create-temp-inplace))
-          (local-file (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-     ; --max-line-length=100 --ignore=E302
-         (list "pep8" (list "--repeat" local-file))))
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 
- (add-to-list 'flymake-allowed-file-name-masks
-              '("\\.py\\'" flymake-pylint-init)))
-
-(defun my-flymake-show-help ()
-  (when (get-char-property (point) 'flymake-overlay)
-    (let ((help (get-char-property (point) 'help-echo)))
-      (if help (message "%s" help)))))
-
-(add-hook 'post-command-hook 'my-flymake-show-help)
-
+;; buck files are actually just Python dicts
 (add-to-list 'auto-mode-alist '("BUCK" . python-mode))
 
 (provide 'setup-python)
