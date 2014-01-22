68481e7b6feb0ce06e7a4644bc72879ebe44d511
Andreas Tolfsen <ato@mozilla.com>
2014-01-22 14:13:38 +0000

    Use a flymake for python that works with tramp

--- emacs.d/setup-python.el
+++ emacs.d/setup-python.el
@@ -8,16 +8,32 @@
 
 ;; On the fly code checking
 (when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
+  (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+      (list "~/.emacs.d/vendor/flymake-python/pyflymake.py" (list local-file))))
+
   (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
+               '("\\.py\\'" flymake-pylint-init)))
+
+;; Activate flymake automatically
 (add-hook 'find-file-hook 'flymake-find-file-hook)
+
+;; ;; On the fly code checking
+;; (when (load "flymake" t)
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list "pyflakes" (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init)))
+;; (add-hook 'find-file-hook 'flymake-find-file-hook)
 
 ;; buck files are actually just Python dicts
 (add-to-list 'auto-mode-alist '("BUCK" . python-mode))
