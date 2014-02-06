a9c7ff05cec54d13d1288bb99aabe548c28dbd8d
Andreas Tolfsen <ato@mozilla.com>
2014-02-06 22:29:34 +0000

    setup-frame-hook is not working as intended
    Very little point to keeping dead, non-functioning code in the tree.

--- emacs.d/defuns/frames.el
+++ emacs.d/defuns/frames.el
@@ -1,14 +1,6 @@
-(defun setup-frame-hook (frame)
-  "This function will be applied to all new Emacs frames."
-  (set-frame-parameter frame 'alpha '(95 95)) ;; translucency
-  (mouse-avoidance-mode 'cat-and-mouse)       ;; avoid mouse
-  (fringe-mode 5)                             ;; make fringes smaller
-  (tool-bar-mode 0)                           ;; no toolbar
-  (menu-bar-mode 0)                           ;; no menubar
-  (set-frame-parameter (selected-frame) 'alpha '(95 95))  ;; translucency
-  (color-theme-charcoal-black))
-
 (defun toggle-fullscreen (&optional f)
+  "Current toggle or (optionally) given frame between fullscreen
+mode."
   (interactive)
   (let ((current-value (frame-parameter nil 'fullscreen)))
     (set-frame-parameter nil 'fullscreen
