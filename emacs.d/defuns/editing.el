7d61b88c0c2314003be73edc8f8c72eca25280be
Andreas Tolfsen <ato@mozilla.com>
2014-02-06 22:33:03 +0000

    Merge save.el and editing.el

--- emacs.d/defuns/editing.el
+++ emacs.d/defuns/editing.el
@@ -1,4 +1,14 @@
-;; Basic text editing defuns
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 (defun create-scratch-buffer nil
   "create a scratch buffer"
