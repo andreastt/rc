036bbafc2b8f2d1f60e1a89ec917337d6b2d51e4
Andreas Tolfsen <ato@mozilla.com>
2014-02-06 22:33:45 +0000

    Merge indentation.el and editing.el

--- emacs.d/defuns/editing.el
+++ emacs.d/defuns/editing.el
@@ -10,6 +10,19 @@ buffer is not visiting a file."
                          (ido-read-file-name "Find file(as root): ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
+(defun indent-all ()
+  "Indents whole buffer and cleans whitespace."
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(defun end-of-line-and-indent-new-line  ()
+  "Jump to end of line, insert new line and indent it."
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+
 (defun create-scratch-buffer nil
   "create a scratch buffer"
   (interactive)
