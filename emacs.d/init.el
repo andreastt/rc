1038d1cd639967f1f8cf6b745f208abef01e9e98
Andreas Tolfsen <ato@mozilla.com>
2014-03-17 18:59:46 +0000

    Timestamp *messages*

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,23 @@
 ;;
 ;;     C-u 0 M-x byte-recompile-directory
 
+;; Time stamp *messages*
+(defun current-time-microseconds ()
+  (let* ((nowtime (current-time))
+         (now-ms (nth 2 nowtime)))
+    (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d] " now-ms))))
+
+(defadvice message (before test-symbol activate)
+  (if (not (string-equal (ad-get-arg 0) "%s%s"))
+      (let ((deactivate-mark nil)
+            (inhibit-read-only t))
+        (save-excursion
+          (set-buffer "*Messages*")
+          (goto-char (point-max))
+          (if (not (bolp))
+              (newline))
+          (insert (current-time-microseconds))))))
+
 ;; Run at full power, please
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
