91f1eedd150de311c1a17997d39983aafe7c41f6
Andreas Tolfsen <ato@mozilla.com>
2014-03-25 12:44:24 +0000

    More drastic reorganization

--- sh/env.sh
+++ sh/env.sh
@@ -1,32 +1,19 @@
-# Plan 9
+# Paths.
 export PLAN9=/usr/local/plan9
 export PATH=$PATH:$PLAN9/bin
-
-# Local bin
 export PATH=$PATH:$HOME/bin
-
-# moz-git-tools
 export PATH=$PATH:$HOME/dev/moz-git-tools
-
-# Go
-export GOROOT=$HOME/dev/go
-
-# Android
 export PATH=$PATH:/usr/local/adt/sdk/platform-tools
 
-# Plumb files instead of starting new editor
+# Plumb files instead of starting new editor, get rid of backspace characters
+# in UNIX man output, and use a free browser!
 export EDITOR=E
 unset FCEDIT VISUAL
-
-# Get rid of backspace characters in UNIX man output
 export PAGER=nobs
-
 export BROWSER=FirefoxNightly
 
-# acme font
+# acme
 export font=$PLAN9/font/lucsans/euro.8.font
-
-# Equivalent variables for rc(1)
 export home=$HOME
 export prompt="$H=;          "
 export user=$USER
@@ -37,17 +24,24 @@ export user=$USER
 #export CCACHE_PREFIX="distcc"
 export DISTCC_HOSTS="+zeroconfig"
 
-# Selenium test suites will continue running even if they reach an error or fail
+# Selenium test suites will continue running even if they reach an error or
+# fail.
 export haltonerror=0
 export haltonfailure=0
 
+# I go by a different name on Githoob.
 export GITHUB_USER="andreastt"
 
-# Python
+# Language specificness.
+#
+# Autoload some Python modules in the REPL and please stop littering my
+# directories with .pyc files.
 export PYTHONSTARTUP=~/.pythonrc.py
 export PYTHONDONTWRITEBYTECODE=1
+export GOROOT=$HOME/dev/go
 
-# Try
+# Mozilla's try server shortcuts because these are almost impossible to
+# remember.
 export TRY_ALL="-b do -p all -u all -t none"
 export TRY_MOBILE="-b do -p emulator,panda -u marionette-webapi -t none"
 export TRY_DESKTOP="-b linux,macosx64,win32,linux_gecko,linux64_gecko -u marionette,marionette-webapi,gaia-ui-test -t none"
