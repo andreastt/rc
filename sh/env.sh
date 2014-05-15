02c16eb74a8c76dd43531916d2a3df3992deacc8
Andreas Tolfsen <ato@mozilla.com>
2014-05-15 14:16:48 +0200

    Format comments

--- sh/env.sh
+++ sh/env.sh
@@ -10,8 +10,8 @@ export PATH=$PATH:$HOME/Code/moz-git-tools
 export PATH=$PATH:/usr/local/adt/sdk/platform-tools
 export PATH=$PATH:$HOME/Code/buck/bin
 
-# Plumb files instead of starting new editor, get rid of backspace characters
-# in Unix man output, and use a free browser!
+# Plumb files instead of starting new editor, get rid of backspace
+# characters in Unix man output, and use a free browser!
 export EDITOR=E
 
 if [[ `uname` == "Linux" ]]; then
@@ -35,8 +35,8 @@ export CCACHE_DIR=$HOME/.config/ccache
 export CCACHE_UMASK=002
 export DISTCC_HOSTS="+zeroconfig"
 
-# Selenium test suites will continue running even if they reach an error or
-# fail.
+# Selenium test suites will continue running even if they reach an
+# error or fail.
 export haltonerror=0
 export haltonfailure=0
 
@@ -45,14 +45,14 @@ export GITHUB_USER=andreastt
 
 # Language specificness.
 #
-# Autoload some Python modules in the REPL and please stop littering my
-# directories with .pyc files.
+# Autoload some Python modules in the REPL and please stop littering
+# my directories with .pyc files.
 export PYTHONSTARTUP=~/.pythonrc.py
 export PYTHONDONTWRITEBYTECODE=1
 export GOROOT=/usr/local/go
 
-# Mozilla's try server shortcuts because these are almost impossible to
-# remember.
+# Mozilla's try server shortcuts because these are almost impossible
+# to remember.
 export TRY_ALL="-b do -p all -u all -t none"
 export TRY_MOBILE="-b do -p emulator,panda -u marionette-webapi -t none"
 export TRY_DESKTOP="-b linux,macosx64,win32,linux_gecko,linux64_gecko -u marionette,marionette-webapi,gaia-ui-test -t none"
