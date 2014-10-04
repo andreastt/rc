33f10018436581c0d4d5a3604498a1014829ba3c
Andreas Tolfsen <ato@mozilla.com>
2014-10-04 15:10:56 +0100

    Remove emacs aliases

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -11,14 +11,3 @@ alias ls="ls -G"
 alias la="ls -ah"
 alias l="ls -lah"
 alias sl=ls
-
-# Commonly used programs
-alias e="emacsclient -c"
-alias en="emacsclient -nw"
-alias ew="emacsclient --no-wait -c"
-
-# Set correct location to system Emacs on OS X
-if [[ "$(uname)" = "Darwin" ]]; then
-    alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
-    alias emacsclient="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
-fi
