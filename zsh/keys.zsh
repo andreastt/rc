e9197eaf9861bd7af00e146bddf4c72e1f7d6e06
Andreas Tolfsen <ato@mozilla.com>
2014-10-04 15:15:13 +0100

    Remove uneeded zsh config options

--- zsh/keys.zsh
+++
@@ -1,3 +0,0 @@
-bindkey "[D" emacs-backward-word  # Meta-<Left>
-bindkey "[C" emacs-forward-word   # Meta-<Right>
-bindkey "^[[3^" delete-word       # C-<Delete>
--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,15 +1,5 @@
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
-
 bindkey "^[m" copy-prev-shell-word
-
 setopt long_list_jobs
-
-# Emacs compatibility
-#setopt No_prompt_cr
-#setopt No_prompt_sp
-
 export LC_CTYPE=en_US.UTF-8
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && \. "$HOME/.rvm/scripts/rvm"
-[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && \. "$HOME/.pythonbrew/etc/bashrc"
