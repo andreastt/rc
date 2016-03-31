71e6fb7f9f163f67230c74eeeb177ea6fb2f6076
Andreas Tolfsen <ato@mozilla.com>
2016-03-31 19:00:41 +0100

    dock: add

---
+++ dock.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dock=$(cat /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-DP-2/status)
+xrandr -q
+
+case "$dock" in
+	"connected")
+	xrandr --verbose \
+		--output LVDS1 --off \
+		--output DP2 --auto
+	;;
+
+	"disconnected")
+	xrandr --verbose --output LVDS1 --auto
+	;;
+esac
+
+FvwmCommand -f "/var/tmp/FvwmCommand-i${DISPLAY}" Restart
+xmodmap ~/.Xmodmap
+
