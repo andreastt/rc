b2c760eff234b88458e213dcd8b920631e4d9a08
Andreas Tolfsen <ato@mozilla.com>
2016-03-31 18:59:38 +0100

    xbindkeysrc: reduce dB on lower/raise volume

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -1,3 +1,3 @@
-(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 6dB+ unmute")
-(xbindkey '("XF86AudioLowerVolume") "amixer set Master 6dB- unmute")
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 3dB+ unmute")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 3dB- unmute")
 (xbindkey '("XF86AudioMute") "amixer set Master toggle")
