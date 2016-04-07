4d3826d199cff40495c46308b1d2b2471066d0f3
Andreas Tolfsen <ato@mozilla.com>
2016-04-07 18:14:46 +0100

    dock: fix script to... work

--- dock.sh
+++ dock.sh
@@ -1,20 +1,60 @@
 #!/bin/sh
 
-dock=$(cat /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-DP-2/status)
-xrandr -q
+# When a udev event fires, the system properties of external output displays
+# are not updated immediately.  This forces us to do poll for which output
+# changed state.
+#
+# This script specifically handles connecting to a ThinkPad dock that is always
+# connected to an extenral display on DP2.  Normally one could facilitate this
+# by listening for docking signals, but for whatever reasons the dock hardware
+# I use only sends events when it is connected; not disconnected.
+#
+# If after 15 seconds it detects that the DP2 connected/disconnected state has
+# not changed, it will abort the script and no harm is done.  If the DP2 state
+# changed, it will take connect- or disconnect actions.
+#
+# I don't normally add a lot of comments to my programs, but the udev system in
+# general is quite mystical.  Consider this an exception.
 
-case "$dock" in
-	"connected")
-	xrandr --verbose \
-		--output LVDS1 --off \
-		--output DP2 --auto
-	;;
+# always enable laptop display first,
+# in case something goes wrong
+xrandr --output LVDS1 --auto
 
-	"disconnected")
-	xrandr --verbose --output LVDS1 --auto
-	;;
+first_state=$(xrandr -q | grep DP2 | awk '{print $2}')
+
+# poll until DP2 changes state or the timeout elapses
+end=$(($(date +%s) + 15))
+while [ $(date +%s) -le $end ]; do
+	second_state=$(xrandr -q | grep DP2 | awk '{print $2}')
+	if [ $first_state != $second_state ]; then
+		break
+	fi
+
+	sleep 1
+done
+
+# if the state on DP2 is still the same as when we began,
+# it means the change event did not originate from DP2
+if [ $first_state = $second_state ]; then
+	exit
+fi
+
+# switch off laptop display and on DP2 if cable was connected,
+# or the reverse if it was disconnected
+case $second_state in
+	connected)
+		xrandr --output DP2 --auto
+		xrandr --output LVDS1 --off
+		;;
+
+	disconnected)
+		xrandr --output LVDS1 --auto
+		xrandr --output DP2 --off
+		;;
 esac
 
-FvwmCommand -f "/var/tmp/FvwmCommand-i${DISPLAY}" Restart
+# reload key map profile as the dock has a keyboard connected,
+# and tell the WM to restart so sticky windows are placed correctly
 xmodmap /home/ato/.Xmodmap
+FvwmCommand -f "/var/tmp/FvwmCommand-$(hostname)${DISPLAY}" Restart
 
