2e87362810bfb01dfa13343245baad299b8e83bb
Andreas Tolfsen <ato@mozilla.com>
2016-05-21 16:03:03 +0100

    xbindkeys: make procedural and add screenshot funcs

--- xbindkeysrc.scm
+++
@@ -1,3 +0,0 @@
-(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 3dB+ unmute")
-(xbindkey '("XF86AudioLowerVolume") "amixer set Master 3dB- unmute")
-(xbindkey '("XF86AudioMute") "amixer set Master toggle")
