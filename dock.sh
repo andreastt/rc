99f531fe935d0ffa15cc0c55908220f9e4ae12ea
Andreas Tolfsen <ato@mozilla.com>
2016-08-04 16:13:54 +0100

    dock: fix screen names

--- dock.sh
+++ dock.sh
@@ -5,12 +5,12 @@
 # changed state.
 #
 # This script specifically handles connecting to a ThinkPad dock that is always
-# connected to an external display on DP3.  Normally one could facilitate this
+# connected to an external display on DP-1.  Normally one could facilitate this
 # by listening for docking signals, but for whatever reasons the dock hardware
 # I use only sends events when it is connected; not disconnected.
 #
-# If after 15 seconds it detects that the DP3 connected/disconnected state has
-# not changed, it will abort the script and no harm is done.  If the DP3 state
+# If after 15 seconds it detects that the DP-1 connected/disconnected state has
+# not changed, it will abort the script and no harm is done.  If the DP-1 state
 # changed, it will take connect- or disconnect actions.
 #
 # I don't normally add a lot of comments to my programs, but the udev system in
@@ -18,14 +18,14 @@
 
 # always enable laptop display first,
 # in case something goes wrong
-xrandr --output LVDS1 --auto
+xrandr --output eDP-1 --auto
 
-first_state=$(xrandr -q | grep DP3 | awk '{print $2}')
+first_state=$(xrandr -q | grep DP-1 | awk '{print $2}')
 
-# poll until DP3 changes state or the timeout elapses
+# poll until DP-1 changes state or the timeout elapses
 end=$(($(date +%s) + 15))
 while [ $(date +%s) -le $end ]; do
-	second_state=$(xrandr -q | grep DP3 | awk '{print $2}')
+	second_state=$(xrandr -q | grep DP-1 | awk '{print $2}')
 	if [ $first_state != $second_state ]; then
 		break
 	fi
@@ -33,23 +33,23 @@ while [ $(date +%s) -le $end ]; do
 	sleep 1
 done
 
-# if the state on DP3 is still the same as when we began,
-# it means the change event did not originate from DP3
+# if the state on DP-1 is still the same as when we began,
+# it means the change event did not originate from DP-1
 if [ $first_state = $second_state ]; then
 	exit
 fi
 
-# switch off laptop display and on DP3 if cable was connected,
+# switch off laptop display and on DP-1 if cable was connected,
 # or the reverse if it was disconnected
 case $second_state in
 	connected)
-		xrandr --output DP3 --auto
-		xrandr --output LVDS1 --off
+		xrandr --output DP-1 --auto
+		xrandr --output eDP-1 --off
 		;;
 
 	disconnected)
-		xrandr --output LVDS1 --auto
-		xrandr --output DP3 --off
+		xrandr --output eDP-1 --auto
+		xrandr --output DP-1 --off
 		;;
 esac
 
