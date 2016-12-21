392d7f9e3698753c859aa8394933d214defd4d80
Andreas Tolfsen <ato@mozilla.com>
2016-12-21 18:15:21 +0000

    env: avoid email

--- env.sh
+++ env.sh
@@ -18,5 +18,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Mozilla's try server shortcuts
 # because these are almost impossible to remember
-export tryall="-b do -e -p all -u all -t none"
-export trymn="-b do -e -p linux64,macosx64,win64,android-api-15 -u xpcshell,marionette,marionette-e10s,web-platform-tests,firefox-ui-functional,firefox-ui-functional-e10s,media-tests,media-youtube-tests,reftest,mochitest -t none"
+export tryall="-b do -p all -u all -t none"
+export trymn="-b do -p linux64,macosx64,win64,android-api-15 -u xpcshell,marionette,marionette-e10s,web-platform-tests,firefox-ui-functional,firefox-ui-functional-e10s,media-tests,media-youtube-tests,reftest,mochitest -t none"
