b95f5a2368220350d9a33b03fd05a6f0cd55ce94
Andreas Tolfsen <ato@sny.no>
2018-08-05 15:40:40 +0100

    env: drop various go entries

--- env.sh
+++ env.sh
@@ -1,11 +1,8 @@
 export PLAN9=/usr/local/plan9
-export GOPATH=$HOME
-export GOBIN=$HOME/bin
 
 export PATH=$HOME/bin:$PATH
 export PATH=$PATH:$PLAN9/bin
 export PATH=$PATH:$HOME/.cargo/bin
-export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$HOME/src/git-bz-moz
 export PATH=$PATH:$HOME/src/git-cinnabar
 
