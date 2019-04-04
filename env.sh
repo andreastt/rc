304c7fd10a3e7ce32fb6eea57bb019aa9bb9d34e
Andreas Tolfsen <ato@sny.no>
2019-04-04 12:57:14 +0100

    Revert "env: drop various go entries"
    This reverts commit b95f5a2368220350d9a33b03fd05a6f0cd55ce94.

--- env.sh
+++ env.sh
@@ -1,6 +1,12 @@
+export PLAN9=/usr/local/plan9
+export GOPATH=$HOME
+export GOBIN=$HOME/bin
+
 export PATH=$HOME/bin:$PATH
 export PATH=$PATH:/usr/local/plan9/bin
 export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$HOME/src/git-bz-moz
 export PATH=$PATH:$HOME/src/git-cinnabar
 export PATH=$PATH:$HOME/src/phlay
 
